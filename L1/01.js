3df7a88493ee4545749b489aeb9778c1966095cb
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-17 14:38:03 +0530

    hello world program

---
+++ L1/01.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello world");
+console.log('yeh bhi chalega xdd no problem ek hi baat hai');
+//note: either use single inverted comma or double both will work.
+// also you can use backticks, but it better to use inverted commas only lol (double in general)
+console.log(`These are called backticks but rarely used to print. so dont. xdd`);
+
+console.log("lets start");
+// shortcut to do comment is cntrl+forward slash 
