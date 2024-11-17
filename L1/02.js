526c9d72af74f0844edb620123ceb240c8d26499
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-17 14:58:15 +0530

    introduction to variables

---
+++ L1/02.js
@@ -0,0 +1,30 @@
+// variables
+"use strict";
+let firstName = "Rishabh";
+var middleName = "Surendra";
+var lastName = "Shukla";
+console.log(firstName);
+
+// note: variable names are case sensitive, so note that   **IMPP**
+// also note that obv variblaes store data that we need to store, and obv we can change that data when req, change kr skte hai tbhi toh naam variable hai. just like maths 
+
+// to change the value
+middleName = "LoneWolf";
+console.log(middleName);    // changed, its that simple
+
+// pagal = 'someone';
+// console.log(pagal);     (since i have written use strict yeh as a var declare nhi honge, and if u remove use strict, obv who as a var declare ho jaenge, But its better to use- use strict as to avoid erros)
+
+//NOTE: for notes/ source code, video ke description me github ki link hai usme hai sb 
+
+var rahul = "100";
+console.log(rahul);
+console.log(rahul + 101); // 100101
+console.log(rahul * 0);   // Js will convert and give 0
+console.log(rahul / 5);   // convert and 20 | etc NOTE for more like these (power, squre root etc etc ckc vdo part 1 from 43:40 )
+console.log(rahul ** 2);
+console.log(rahul ** 0.5);   // squre root obv 
+
+var value34 = '369';
+console.log(value34);
+
