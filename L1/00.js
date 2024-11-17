a03c9bdae8ae3b2e041add1c008fdb5d64896db9
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-17 14:24:31 +0530

    syllabus

---
+++ L1/00.js
@@ -0,0 +1,94 @@
+//  Introduction 
+//  Syllabus
+//  History of JavaScript
+//  Download and Install vscode
+//  How to execute javascript code
+//  Hello World program
+//  Intro to variables
+//  Rules for naming variables
+//  Let keyword
+//  Const keyword 
+//  String Indexing
+//  Useful string methods
+//  typeof , string to number, number to string conversion
+//  String Concatenation
+//  Template Strings
+//  undefined null bigint
+//  Booleans and comparison Operators
+//  if else , truthy and falsy values
+//  Ternary operator
+//  And Or Operator
+//  Nested If else
+//  if elseif 
+//  Switch statement
+//  While loop
+//  While loop example
+//  Intro to for loop
+//  for loop examples
+//  Break and Continue Keyword
+//  Do while loop
+//  Intro to arrays
+//  push pop shift unshift
+//  Primitive vs reference data types
+//  Clone array, concat, spread operator
+//  For loop in array
+//  Use const for creating arrays
+//  While loop in array
+//  For of loop
+//  For in loop
+//  Array destructuring
+//  Intro to objects
+//  Dot vs bracket notation
+//  How to iterate objects
+//  Computed Properties
+//  Spread operator in array and objects
+//  Object destructuring
+//  Object inside array
+//  Nested destructuring
+//  Function declaration
+//  Function Expression
+//  Arrow Functions
+//  Hoisting intro ( later covered in detail) 
+//  Functions inside functions
+//  Lexical Scope
+//  Block scope vs function scope
+//  Default parameters
+//  Rest parameters
+//  Param destructuring
+//  callback functions
+//  Function returning function
+//  forEach method
+//  map method
+//  filter method
+//  reduce method
+//  sort method
+//  find method
+//  every method
+//  some method
+//  fill method
+//  splice method
+//  Iterables, array like objects
+//  sets 
+//  map data structure
+//  clone using Object.assign
+//  optional chaining
+//  Create your own methods
+//  This keyword
+//  call, apply, bind methods
+//  small warning
+//  arrow functions and this
+//  short syntax
+//  create functions to create multiple objects
+//  store methods in different object
+//  solution using object.create
+//  What is prototype
+//  Use prototype
+//  New keyword
+//  hasOwnProperty
+//  more about prototype
+//  class keyword
+//  class practice and extends keyword
+//  super keyword
+//  same method in base class
+//  getters and setters
+//  static methods and properties
