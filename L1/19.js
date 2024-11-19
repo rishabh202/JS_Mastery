759726eaf758f66c16023cfc985f8dd65112276a
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-19 22:45:13 +0530

    do while loop

---
+++ L1/19.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// do while loop
+// first see normal while loop
+let s=0; 
+while(s<5){
+    console.log(s); 
+    s++; 
+}   // 0 se 4 tk print kr dega
+
+console.log("");
+
+let o=0; 
+do{
+    console.log(o);   // 0 se 7 tk print kr dega
+    o++; 
+} while(o<8);  //Check video for better understanding, while loop condition false hone pe nhi chlega while, do while ek baar chl jagea, see video for why and rmbr and master
+
+// do while loop condition false hone pe bhi ek baar chalega
+
+let p= 56;
+do{
+    console.log(p);
+    p++;
+}while(p<=10);  // see ek baar print hogya
