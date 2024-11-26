8234f49cb51e9f86b05668b39787e19266b6acf8
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-11-26 11:46:45 +0530

    same method in base class

---
+++ L1/76.js
@@ -0,0 +1,42 @@
+"use strict";
+// same method in base class
+class Bird{
+    constructor(name, age){
+          this.name = name;
+          this.age = age;
+    }
+
+    eat(){
+        return `${this.name} is eating`;
+    }
+
+    isCute(){
+        return this.age<=3;
+    }
+
+    isHuman(){
+        return true;
+    }
+}
+
+class eagle extends Bird{
+     constructor(name, age, speed){
+        super(name, age);
+        this.speed = speed
+     }
+
+     run(){
+        return `${this.name} can run at ${this.speed}mph`
+     }
+
+     eat(){
+        return `Modified eating`
+     }
+}
+
+
+const eagle1 = new eagle("eagleOne", 23, 1000);
+console.log(eagle1);
+console.log(eagle1.run());
+console.log(eagle1.eat()); 
+// first js will check in base class, if not present then in parent class
