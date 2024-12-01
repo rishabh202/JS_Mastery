7fbcca452523435b7a735d91456ca36482d5a65a
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-01 19:51:30 +0530

    introduction to global execution context

---
+++ L2/02.js
@@ -0,0 +1,20 @@
+// "use strict";
+
+// global execution context
+
+console.log(this);
+console.log(window);
+console.log(typeof window);
+console.log(firstName);
+console.log(typeof this);
+var firstName = "Rishabh"
+console.log(firstName);
+
+// js is synchoronous programming language i.e jbtk phele like execute nai hoti, dusri nai hoga,
+// and js have single thread
+console.log(" gap ");
+console.log(this);
+console.log(window);
+console.log(lastName);
+var lastName = "shukla"
+console.log(lastName);
