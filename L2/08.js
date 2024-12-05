c152eb17e71d671fbcc197dd10aef14832765736
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-05 10:24:05 +0530

    some revision about global execution context

---
+++ L2/08.js
@@ -0,0 +1,15 @@
+"use strict";
+// some revision
+
+
+console.log("helllow word");
+let firstName = "Rishabh";
+let lastName = "Shukla"; 
+
+const myfunc = function(){
+    let var1 = 'First Variable';
+    let var2 = "second var";
+    
+}
+
+// u can aslo check the lecture or points u have saved in telegram
