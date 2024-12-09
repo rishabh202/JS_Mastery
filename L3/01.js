4184c9b1dbbfc1a87a638e2e9dc47ff2fc4d593d
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-09 19:30:43 +0530

    Very basic intro to DOM

---
+++ L3/01.js
@@ -0,0 +1,12 @@
+"use strict";
+// DOM tut
+// overview
+// how to use 
+// deep study
+
+// Browser creates a document object to read the html or other files
+console.log(window.document);  // ye bhi dir jaese hi dikh raha hai
+console.dir(window.document);
+console.dir(document);
+// we can use document to edit events.
+// scroll, submit etc are some of the example of events
