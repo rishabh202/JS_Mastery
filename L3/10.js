9d231d15d4ad42d4c4f86d314248e41623bbe2f5
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-11 16:06:09 +0530

    innerHTML

---
+++ L3/10.js
@@ -0,0 +1,17 @@
+"use strict";
+// innerHTML
+
+// kisi bhi component k andar ka cheez innerHTML hai
+const headLine = document.querySelector(".headlines");
+console.log(headLine);
+console.log(headLine.innerHTML);
+
+// see even comments are shown in innerHTML
+
+
+headLine.innerHTML = "<h2> InnerHTML changed</h2>"
+headLine.innerHTML += "<button class=\"btn\"> clickme </button>"
+// "" and andar "" aise use krte hai \" \"
+// i.e \" ----> "
+headLine.innerHTML += "<button> extraBtn </button>"
+// headLine.innerHTML -= "<button> extraBtn </button>"
