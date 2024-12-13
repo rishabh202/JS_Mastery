ef7260d28f09d4b77c866c07d30e59d02877011c
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-13 13:13:33 +0530

    traversing through dom, deep understanding to nodes, children etc

--- L4/02.js
+++ L4/02.js
@@ -5,8 +5,10 @@
 const rootNode = document.getRootNode();
 console.log(rootNode);
 console.log(rootNode.childNodes);
+// node list is an array like object, so we can se indexing
 const htmlElementNode = rootNode.childNodes[0];
 console.log(htmlElementNode);
+console.dir(htmlElementNode);
 
 
 console.log(htmlElementNode.childNodes);
@@ -15,31 +17,75 @@ console.log(htmlElementNode.childNodes);
 const headElementNode = htmlElementNode.childNodes[0];
 console.log(headElementNode);
 
+
 const textNode = htmlElementNode.childNodes[1];
 console.log(textNode);
 
-const bodyElementNode = htmlElementNode[2];
+// 
+console.log("");
+
+const bodyElementNode = htmlElementNode.childNodes[2];
 console.log(bodyElementNode);
 
 
 // parent relationship 
+console.log("parent")
 console.log(headElementNode.parentNode);
 
 // ~ly etc 
 
 // sibling relationships
+
+console.log("sib")
 console.log(headElementNode.nextSibling);
-// console.log(headElementNode.nextSibling.nextSibling);
+console.log(headElementNode.nextSibling.nextSibling);
+console.log(headElementNode.nextSibling.nextSibling.nextSibling);  //null
+console.log(headElementNode.nextElementSibling);  // body hi de dega
+// next element sibling ignore the empty texts
 
 
 // browser uses document to create the ui 
 
 // white space normal hone ki wjh se we dont get to see extra space.
+// if we use white-space pre than extra places will be shown,
 
-// to ignore the text we can use like
-// ig these things are changed/updated in latest version of js 
-// so skip as of now
-
+console.log("");
 console.log(headElementNode.childNodes)
 
-// skipped this part from 5:07:00
+// 
+// to change or edit using js/dom
+const h1 = document.querySelector("h1");
+console.log(h1);
+console.log(h1.parentNode);
+const h1Parent = h1.parentNode;
+h1Parent.style.color = "#efefef";  
+h1Parent.style.backgroundColor = "#333"
+
+// now ~ly for etc also
+const body = h1.parentNode.parentNode;
+console.log(body);
+body.style.backgroundColor = "yellow"
+
+//
+// u can also directly select body by like 
+// const body = document.body
+// console.log(body);
+// body.style.backgroundColor = "gray"  // commented out as it is now redundant, hence wont work
+
+//
+// hum querySelector k andr bhi querySelector use kr skte hai
+console.log(" ");
+const head = document.querySelector("head");
+// console.log(head);  // we have to comment this for next to happen. as of now idk why? lol
+const title = head.querySelector("title")
+console.log(title);
+console.log(title.childNodes);
+
+
+// suppose we dont want text
+const container = document.querySelector(".container") 
+console.log(container);  // suppose we dont want the empty texts, so we can like
+console.log(container.childNodes);  
+console.log(container.children);   // thats how it is done
+
+// children above. 
