f1e723730fddf4270af216f28ff7f3865328a758
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-14 09:17:23 +0530

    createElements(): append

--- L6/02.js
+++ L6/02.js
@@ -1,17 +1,14 @@
 "use strict";
 // document.createElement()    to add.
 
-const newTodoItem = document.createElement("li");
-// const newTodoItemText = document.createTextNode("Teach students");
-newTodoItem.textContent = "Teach the learner";
+const newTodoItem  = document.createElement("li");
+const newTodoItemText = document.createTextNode("Teach anyone")
+ console.log(newTodoItem);  // the li element has been created
+// now we want to add newTodoItemText to newTodoItem
+// so 
+newTodoItem.appendChild(newTodoItemText);
+// now we want it to be on webpage
 const todoList = document.querySelector(".todo-list");
-// todoList.append(newTodoItem);  // adds to last
-todoList.prepend(newTodoItem);   // adds to first
-console.log(newTodoItem)
-//also like 
-
-
-// we can also remove
-const todo1 = document.querySelector(".todo-list li")
-todo1.remove();
-console.log
+// todoList.append(newTodoItemText); // nnow it will show on ui also, bt isses hamari css nai aaega
+newTodoItem.append(newTodoItemText);
+todoList.append(newTodoItem);
