cf20a10423c1f3247f0cc6c6ef2b268402e9f19e
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-12-17 21:18:36 +0530

    demo mini project

--- L7/7.js
+++ L7/7.js
@@ -2,6 +2,8 @@
 // little demo project
 
 const mainButton = document.querySelector("button");
+const currentColor = document.querySelector(".current-color")
+console.log(currentColor);
 function randomColorGenerator(){
   const red = Math.floor(Math.random()*256)
   const green = Math.floor(Math.random()*256)
@@ -12,7 +14,8 @@ function randomColorGenerator(){
 const body = document.body // to change the background
 mainButton.addEventListener("click", ()=>{
     const randomColor = randomColorGenerator();
-    console.log(randomColor);
+   body.style.backgroundColor = randomColor;
+   currentColor.textContent = randomColor
 })
 
 // see rgb me teeno ki value 0 se leke 255 tak jati hai
